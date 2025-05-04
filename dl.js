e56d4bf61038fdb5fcc2705d5b7e528eadcebd39
gorenganhunter <gorengandev@gmail.com>
2025-05-04 10:29:37 +0700

    bruh

---
+++ dl.js
@@ -0,0 +1,20 @@
+const form = document.body.querySelector("form")
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const id = document.forms["dl"]["chartid"].value
+    document.forms["dl"]["chartid"].value = ""
+    
+    fetch("https://api.phira.cn/chart/" + id)
+        .then(res => res.json())
+        .then(data => {
+            const a = document.createElement("a")
+            a.style.display = "none"
+            a.href = data.file
+            a.download = data.name + ".zip"
+            document.body.appendChild(a)
+            a.click()
+            a.remove()
+        })
+        .catch(err => alert(err))
+})
